81bd0a081c6f969f971a0fd01da24ccb098d2498
svp95bos <36014542+svp95bos@users.noreply.github.com>
2024-12-08 11:37:19 +0100

    Lade till ny lösning och klass för LED-styrning
    1. Lade till en ny Visual Studio-lösningsfil `PhilipsSignageDisplaySicp.sln`. Denna fil specificerar lösningsformatversionen, Visual Studio-versionen och inkluderar konfigurationsdetaljer för projektet `PhilipsSignageDisplaySicp`. Den definierar också lösningskonfigurationer för Debug- och Release-lägen samt en lösnings-GUID.
    2. Lade till en ny C#-klassfil `Philips10BDL4151TClient.cs`. Denna fil definierar klassen `Philips10BDL4151TClient` som ärver från `PhilipsSicpClient`. Klassen inkluderar metoder för att aktivera och inaktivera en LED-remsa (`EnableLedStrip` och `DisableLedStrip` respektive) samt en metod för att hämta den aktuella statusen för LED-remsan (`GetLedStrip`). Klassen använder `SicpSocket` för kommunikation och interagerar med `SicpCommands` för LED-styrning.

---
+++ PhilipsSignageDisplaySicp/Philips10BDL4151TClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+using PhilipsSignageDisplaySicp.Models;
+
+namespace PhilipsSignageDisplaySicp
+{
+    public class Philips10BDL4151TClient
+       : PhilipsSicpClient
+    {
+        public Philips10BDL4151TClient(SicpSocket socket, byte monitorId = 1, byte groupId = 0)
+            : base(socket, monitorId, groupId) { }
+
+        public virtual void EnableLedStrip(Color color)
+        {
+            Set(SicpCommands.LedControlSet, true.ToByte(), color.R, color.G, color.B);
+        }
+
+        public virtual void DisableLedStrip()
+        {
+            Set(SicpCommands.LedControlSet, false.ToByte(), 0, 0, 0);
+        }
+
+        public virtual LedStripResult GetLedStrip()
+        {
+            return Get<LedStripResult>(SicpCommands.LedControlGet);
+        }
+    }
+}
